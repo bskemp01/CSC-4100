6874d2d21c6568359bb024713079ce0429c01b7b
bskemp01 <bskemp01@gmail.com>
2022-01-31 21:08:06 -0600

    kernel.asm not working now. pushing to show code to TA

--- kernel.asm
+++ kernel.asm
@@ -9,32 +9,33 @@ k_print:
 	push esi
 	push edi
 
-	mov ebx, [ebp + 16]	;Move column into ebx
-	mov eax, [ebp + 12]	;Move row into eax
-	mov esi, [ebp + 8]	;Move address of string into esi
+	mov ebx, [ebp + 20]	
+	mov eax, [ebp + 16]	
+	mov esi, [ebp + 8]	
 
 	;Calculate offset using equation
 	;offset = 0xB8000 + 2(row*80 + column)
 
-	imul eax, 80		;row*80
-	add eax, ebx		;row*80 + column
-	imul eax, 2			;2(row*80 + column)
-	add eax, 0xB8000	;0xB8000 + 2(row*80 + column)
-	mov edi, eax		;Move offset into edi
+	imul eax, 80		
+	add eax, ebx		
+	imul eax, 2			
+	add eax, 0xB8000	
+	mov edi, eax		
+    mov esi, [ebp +8]
+    
+    loop:
+        cmpb esi, 0
+        je done
+        movsb 	
+        movb es:[edi], 31
+        inc edi
+        jmp loop
 
-loop:
-	cmpb [esi], 0
-	je done
-	movsb 	
-	movb es:[edi], 10
-	inc edi
-	jmp loop
-
-done:
-	pop edi
-	pop esi
-	pop ebx
-	pop eax
-	popfd
-	pop ebp
-	ret
+    done:
+        pop edi
+        pop esi
+        pop ebx
+        pop eax
+        popfd
+        pop ebp
+        ret
